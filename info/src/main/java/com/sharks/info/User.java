f8359ea1a2a2eee8ff0c67e63f31764b96292e95
Venkatesh G <gvenka@google.com>
2025-02-25 11:22:47 +0530

    added

--- info/src/main/java/com/sharks/info/User.java
+++ info/src/main/java/com/sharks/info/User.java
@@ -2,22 +2,22 @@ package com.sharks.info;
 
 public class User {
 
-    private int id;
+    private int id1;
     private String firstName;
     private String lastName;
     private String email;
 
     public User() {}
 
-    public User(int id, String firstName,String lastName, String email) {
-        this.id = id;
+    public User(int id1, String firstName,String lastName, String email) {
+        this.id1 = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId1() { return id1; }
+    public void setId1(int id1) { this.id1 = id1; }
     
     public String getfirstName() { return firstName; }
     public void setName(String firstName) { this.firstName = firstName; }
